c0d89182d59ca5d758326aa84985b6e29bb2d045
miguelarial <mikearial@icloud.com>
2024-11-26 23:09:46 +0000

    Initial commit for arialmed project

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,44 @@
+"use server"
+
+import RegisterForm from '@/components/forms/RegisterForm'
+import { getUser } from '@/lib/actions/patient.actions'
+import Image from 'next/image'
+
+import * as Sentry from '@sentry/nextjs'
+
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+    const user = await getUser(userId);
+
+    // used for tracking the number of users that viewed a page.
+    Sentry.metrics.set("user_view_register", user.name);
+
+    return (
+        <main className="flex h-screen max-h-screen">
+            <section className="remove-scrollbar container">
+                <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+                    <Image
+                        src="/assets/icons/logo-full.svg"
+                        height={1000}
+                        width={1000}
+                        alt="Logo Image"
+                        className="mb-12 h-10 w-fit"
+                    />
+
+                    <RegisterForm user={user} />
+
+                    <p className="copyright py-12">© 2024 Arialmed</p>
+                </div>
+            </section>
+
+            <Image
+                src="/assets/images/register-img.png"
+                width={1000}
+                height={1000}
+                alt="Trusted doctor"
+                className="side-img max-w-[390px]"
+            />
+        </main>
+    )
+}
+
+export default Register
