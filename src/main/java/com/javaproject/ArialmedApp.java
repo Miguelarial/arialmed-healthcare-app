d090929acdca3650e22905f35b9deff828350a81
miguelarial <mikearial@icloud.com>
2024-11-30 00:39:08 +0000

    Changes made in the pom.xml file

---
+++ src/main/java/com/javaproject/ArialmedApp.java
@@ -0,0 +1,12 @@
+package com.javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArialmedApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArialmedApp.class, args);
+    }
+} 
