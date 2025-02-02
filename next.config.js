c5f779030dc4700a30cb2ad4257b061445dbc3ef
miguelarial <mikearial@icloud.com>
2025-02-02 12:10:39 +0000

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: undefined,
+  },
+  env: {
+    NEXT_PUBLIC_ADMIN_PASSKEY: process.env.NEXT_PUBLIC_ADMIN_PASSKEY || '',
   }
 }
 
